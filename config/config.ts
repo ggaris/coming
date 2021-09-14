67d8e5b90ecec195140cde884e36db9871be8dab
“左洋” <zuoyang@baijiayun.com>
2021-09-14 09:33:03 +0800

    ✨ feat(webpack): 添加支持postcss

--- config/config.ts
+++ config/config.ts
@@ -1,4 +1,26 @@
 // 配置文件
 export default {
-    // 具体配置项
-  };
+  // 具体配置项
+  module:{
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
--- src/Button/index.md
+++ src/Button/index.md
@@ -7,12 +7,12 @@ import { Button } from "coming";
 const ButtonDemo = () => {
   return (
     <div>
-      <Button type='primary'>标准按钮</Button>
-      <hr />
-
       <Button type='default'>默认按钮</Button>
       <hr />
-      <Button type='danger'>无边框按钮</Button>
+      <Button type='primary'>主要按钮</Button>
+      <hr />
+
+      <Button type='danger'>危险按钮</Button>
       <hr />
       <Button type='link'>这是透明按钮</Button>
     </div>
