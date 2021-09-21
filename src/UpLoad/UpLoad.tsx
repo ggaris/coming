05d38c01208e038469b5302b4ba86841b1bf0d2b
“左洋” <zuoyang@baijiayun.com>
2021-09-21 23:06:24 +0800

    ✨ feat(UpLoad):
    上传文件以及回显

--- src/UpLoad/UpLoad.tsx
+++ src/UpLoad/UpLoad.tsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./UpLoad.less";
 interface UpLoadProps extends InheritableClassName {}
 const UpLoad: React.FunctionComponent<UpLoadProps> = props => {
+  const InputEl = useRef<HTMLInputElement>(null);
+  const [Files, setFiles] = useState<Array<File>>();
+  const upLoad = () => {
+    InputEl.current!.click();
+    console.log(Files);
+  };
+  const upFiles = () => {
+    if (InputEl.current!.files !== null) {
+      let files = [];
+      let len = InputEl.current!.files.length;
+      for (let i = 0; i < len; i++) {
+        files.push(InputEl!.current!.files[i]);
+      }
+      setFiles(files);
+    }
+  };
+
   return (
     <>
-      <div className={`cui-upload ${props.className}`}></div>
-      <input className='cui-upload-input' type='file' accept='image/*' />
+      <div className='cui-upload'>
+        {Files?.map(e => {
+          return (
+            <div key={e.name} className='cui-upload-show'>
+              <img src={URL.createObjectURL(e)} />
+              <span>{e.name}</span>
+            </div>
+          );
+        })}
+        <div
+          className={`cui-upload-button ${props.className}`}
+          onClick={() => upLoad()}></div>
+      </div>
+      <input
+        className='cui-upload-input'
+        type='file'
+        accept='image/*'
+        multiple
+        onChange={() => upFiles()}
+        ref={InputEl}
+      />
     </>
   );
 };
