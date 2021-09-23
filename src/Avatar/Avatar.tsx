dc572c1ff7282746345c12cd1e26ceba97ec8a6c
“左洋” <zuoyang@baijiayun.com>
2021-09-23 19:49:08 +0800

    ✨ feat:
    Avatar文档

--- src/Avatar/Avatar.tsx
+++ src/Avatar/Avatar.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Avatar.less";
 
-type Size = "large" | "small" | "default";
+type Size = "large" | "small" | "default"; //头像大小
+type Shape = "circle" | "square"; // 头像样式
+type Src = string; // 头像引用地址
 interface AvatarProps extends InheritableClassName {
   size?: Size;
+  shape?: Shape;
+  src?: Src;
 }
 const Avatar: React.FunctionComponent<AvatarProps> = props => {
   return (
@@ -11,7 +15,7 @@ const Avatar: React.FunctionComponent<AvatarProps> = props => {
       className={`cui-avatar ${props.className} ${
         props.size ? props.size : ""
       }`}>
-      asgasgasg
+      {props.src ? <img src={props.src}></img> : null}
     </div>
   );
 };
