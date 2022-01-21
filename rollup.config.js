069850197440e749b7a7e18500e994f7588ace87
aris <61764593+jiantongCode@users.noreply.github.com>
2022-01-21 12:00:37 +0800

    ✨ feat(依赖): 添加支持postcss (#12)

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
+import postcss from "rollup-plugin-postcss";
 
 const packageJson = require("./package.json")
 
@@ -26,12 +27,14 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
+      postcss()
     ],
     external: ["react", "typescript"],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()]
+    plugins: [dts()],
+    external: [/\.css$/]
   }
 ]
