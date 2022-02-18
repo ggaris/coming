66e0999f4a6588493cb7d5f600103d5b5a469602
jiantongCode <jiantong12138@163.com>
2022-02-18 20:02:06 +0800

    feat: rollup

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
 import postcss from "rollup-plugin-postcss";
 
-const packageJson = require("./package.json")
+import packageJson from './package.json'
 
 
 export default [
