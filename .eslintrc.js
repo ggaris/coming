a6446e93e1600e104639f5849112078240d11a20
jiantongCode <jiantong12138@163.com>
2022-02-18 17:42:51 +0800

    fix(commit): 修改配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   ],
   env: {
     // 你的环境变量（包含多个预定义的全局变量）
-    //
     browser: true,
     // node: true,
     // mocha: true,
@@ -15,7 +14,6 @@ module.exports = {
   },
   globals: {
     // 你的全局变量（设置为 false 表示它不允许被重新赋值）
-    //
     myGlobal: false
   },
   rules: {
