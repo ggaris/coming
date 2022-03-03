f2d7b3f40804022f09775001b587a2836e708c4c
jiantongCode <jiantong12138@163.com>
2022-03-03 15:11:29 +0800

    feat: 🍺添加Button 基本测试

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  testEnvironment: "jsdom",
+  testEnvironment: 'jest-environment-jsdom',
   collectCoverage: true,
+  roots: ['<rootDir>/src'],
+  testRegex: '^.+\\.test\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     ".(css|scss|sass)$": "identity-obj-proxy"
   }
